770b6299686b8acf868f24a43c199030d76cd8d3
maciejsidor@gmail.com <maciejsidor@gmail.com@af7a9ef3-35a2-7c67-2c85-79ad0fb73ffd>
2014-03-27 23:49:37 +0000

    Share project "hpalm-maven-plugin" into "https://hpalm-maven-plugin.googlecode.com/svn"

---
+++ trunk/src/main/java/com/sim/hpalm/defects/Entity.java
@@ -0,0 +1,21 @@
+package com.sim.hpalm.defects;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name="Entity")
+public class Entity 
+{
+	@XmlElement(name="Field")
+	@XmlElementWrapper(name="Fields")
+	public List<Field> fields = null;
+
+	@XmlAttribute
+	public String type= null;
+
+	public String id;
+}
