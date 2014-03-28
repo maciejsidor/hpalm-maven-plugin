5bd234e39d10f34e4357edcd39b463dcac5ed3c4
maciejsidor@gmail.com <maciejsidor@gmail.com@af7a9ef3-35a2-7c67-2c85-79ad0fb73ffd>
2014-03-28 02:58:16 +0000

    [maven-release-plugin]  copy for tag release-0.0.2

---
+++ tags/release-0.0.2/src/main/java/com/googlecode/msidor/maven/plugins/hpalm/deliverynote/Entity.java
@@ -0,0 +1,41 @@
+/*
+Copyright 2014 Maciej SIDOR [maciejsidor@gmail.com]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.	
+ */
+package com.googlecode.msidor.maven.plugins.hpalm.deliverynote;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author Maciej SIDOR
+ * 
+ * JAXB object representing defect
+ */
+@XmlRootElement(name="Entity")
+public class Entity 
+{
+	@XmlElement(name="Field")
+	@XmlElementWrapper(name="Fields")
+	public List<Field> fields = null;
+
+	@XmlAttribute
+	public String type= null;
+
+	public String id;
+}
